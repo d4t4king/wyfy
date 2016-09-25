665892ae15db68e1ca22beadb283ffb6bc04612d
dataking <dataking@gmail.com>
2016-09-25 14:37:45 -0700

    test_wc.pl fleshed out

--- test_wc.pl
+++ test_wc.pl
@@ -28,18 +28,85 @@ if ((!defined($input)) or ($input eq "")) {
 
 my $xdoc = XMLin($input);
 
-print Dumper($xdoc->{'wireless-network'}[0]{'wireless-client'}[0]);
+if (ref($xdoc->{'wireless-network'}) eq 'ARRAY') {
+	foreach my $wnet ( @{$xdoc->{'wireless-network'}} ) {
+		if (ref($wnet->{'wireless-client'}) eq 'ARRAY') {
+			foreach my $wc ( @{$wnet->{'wireless-client'}} ) {
+				print Dumper($wc) if ($verbose);
+				my $wcli = Wireless::Client->new($wc->{'client-mac'}, $wc);
+				print Dumper($wcli) if ($verbose);
+				print "#" x 80; print "\n";
+				printf "%-40s %-s\n", "MAC Address:", $wcli->mac_address;
+				printf "%-40s %-s\n", "First Time:", $wcli->first_time;
+				printf "%-40s %-s\n", "Last Time:", $wcli->last_time;
+				printf "%-40s %-s\n", "Channel:", $wcli->channel;
+				printf "%-40s %-s\n", "Type:", $wcli->type;
+				printf "%-40s %-s\n", "Packet Fragments:", $wcli->packet_fragments;
+				printf "%-40s %-s\n", "Crypto Packets:", $wcli->crypto_packets;
+				printf "%-40s %-s\n", "Packet Retries:", $wcli->packet_retries;
+				printf "%-40s %-s\n", "Data Packets:", $wcli->data_packets;
+				printf "%-40s %-s\n", "LLC Packets:", $wcli->llc_packets;
+				printf "%-40s %-s\n", "Total Packets:", $wcli->total_packets;
+				printf "%-40s %-s\n", "Manufacturer:", $wcli->manufacturer;
+				printf "%-40s %-s\n", "Number:", $wcli->number;
+				printf "%-40s %-s\n", "Max Seen Rate:", $wcli->max_seen_rate;
+			}
+		} else {
+			next if (!defined($wnet->{'wireless-client'}));
+			if (ref($wnet->{'wireless-client'}) eq 'HASH') {
+				print Dumper($wnet->{'wireless-client'}) if ($verbose);
+				#my $wcli = Wireless::Client->new($wnet->{'wireless-client'}{'client-mac'}, { number => $xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'number'}, type => $xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'type'} });
+				my $wcli = Wireless::Client->new($wnet->{'wireless-client'}{'client-mac'}, $wnet->{'wireless-client'});
+				print Dumper($wcli) if ($verbose);
+				print "#" x 80; print "\n";
+				printf "%-40s %-s\n", "MAC Address:", $wcli->mac_address;
+				printf "%-40s %-s\n", "First Time:", $wcli->first_time;
+				printf "%-40s %-s\n", "Last Time:", $wcli->last_time;
+				printf "%-40s %-s\n", "Channel:", $wcli->channel;
+				printf "%-40s %-s\n", "Type:", $wcli->type;
+				printf "%-40s %-s\n", "Packet Fragments:", $wcli->packet_fragments;
+				printf "%-40s %-s\n", "Crypto Packets:", $wcli->crypto_packets;
+				printf "%-40s %-s\n", "Packet Retries:", $wcli->packet_retries;
+				printf "%-40s %-s\n", "Data Packets:", $wcli->data_packets;
+				printf "%-40s %-s\n", "LLC Packets:", $wcli->llc_packets;
+				printf "%-40s %-s\n", "Total Packets:", $wcli->total_packets;
+				printf "%-40s %-s\n", "Manufacturer:", $wcli->manufacturer;
+				printf "%-40s %-s\n", "Number:", $wcli->number;
+				printf "%-40s %-s\n", "Max Seen Rate:", $wcli->max_seen_rate;
+			} else {
+				warn colored("Wireless client object not an array or hash!", "bold red"); print "\n";
+				print color('bold red');
+				print ref($wnet->{'wireless-client'})."\n";
+				print Dumper($wnet->{'wireless-client'});
+				print color("reset");
+				exit 255;
+			}
+		}
+	}
+} else {
+	warn colored("Wireless network object not an array! \n", "bold red");
+	print Dumper($xdoc->{'wireless-network'});
+	exit 254;
+}
+#print Dumper($xdoc->{'wireless-network'}[0]{'wireless-client'}[0]) if ($verbose);
 #my $wcli = Wireless::Client->new($xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'client-mac'}, { number => $xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'number'}, type => $xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'type'} });
-my $wcli = Wireless::Client->new($xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'client-mac'}, \%{$xdoc->{'wireless-network'}[0]{'wireless-client'}[0]});
-print Dumper($wcli);
-print "The mac address for the found client is ".$wcli->mac_address."\n";
-#my %keys;
-#foreach my $k ( sort keys %{$xdoc} ) {
-#	$keys{$k}++;
-#}
-#foreach my $k ( sort { $keys{$b} <=> $keys{$a} } %keys ) {
-#	print "$k $keys{$k}\n";
-#}
+#my $wcli = Wireless::Client->new($xdoc->{'wireless-network'}[0]{'wireless-client'}[0]{'client-mac'}, \%{$xdoc->{'wireless-network'}[0]{'wireless-client'}[0]});
+#print Dumper($wcli) if ($verbose);
+#print "#" x 80; print "\n";
+#printf "%-40s %-s\n", "MAC Address:", $wcli->mac_address;
+#printf "%-40s %-s\n", "First Time:", $wcli->first_time;
+#printf "%-40s %-s\n", "Last Time:", $wcli->last_time;
+#printf "%-40s %-s\n", "Channel:", $wcli->channel;
+#printf "%-40s %-s\n", "Type:", $wcli->type;
+#printf "%-40s %-s\n", "Packet Fragments:", $wcli->packet_fragments;
+#printf "%-40s %-s\n", "Crypto Packets:", $wcli->crypto_packets;
+#printf "%-40s %-s\n", "Packet Retries:", $wcli->packet_retries;
+#printf "%-40s %-s\n", "Data Packets:", $wcli->data_packets;
+#printf "%-40s %-s\n", "LLC Packets:", $wcli->llc_packets;
+#printf "%-40s %-s\n", "Total Packets:", $wcli->total_packets;
+#printf "%-40s %-s\n", "Manufacturer:", $wcli->manufacturer;
+#printf "%-40s %-s\n", "Number:", $wcli->number;
+#printf "%-40s %-s\n", "Max Seen Rate:", $wcli->max_seen_rate;
 
 ###############################################################################
 # Subs
