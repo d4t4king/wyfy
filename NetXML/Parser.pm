51dc55c3d14b27760665eff5ab803348b360430c
d4t4king <dataking@gmail.com>
2017-01-23 11:48:01 -0800

    add more debugging output.

--- NetXML/Parser.pm
+++ NetXML/Parser.pm
@@ -2,6 +2,10 @@ package NetXML::Parser;
 
 use strict;
 use warnings;
+#use experimental 'smartmatch';
+use feature qw( switch );
+no if $] >= 5.017011, warnings => 'experimental::smartmatch';
+
 use Term::ANSIColor;
 use Data::Dumper;
 use XML::Simple;
@@ -34,18 +38,42 @@ sub parsefile {
 					$self->{'source-card'} = $card;
 				} else {
 					warn colored("card source object not a hash!", "yellow");
+					print color('yellow');
+					print Dumper($xdoc->{'card_source'});
+					print color('reset');
 				}
 			}
-            if (ref($xdoc->{'wireless-network'}) eq 'ARRAY') {
-                foreach my $net ( @{$xdoc->{'wireless-network'}} ) {
-                    my $wnet;
-                    if (!defined($net->{'SSID'}{'essid'}{'content'})) {
-                        $wnet = NetXML::Wireless::Network->new($net->{'BSSID'}, "Unknown", $net);
-                    } else {
-                        $wnet = NetXML::Wireless::Network->new($net->{'BSSID'}, $net->{'SSID'}{'essid'}{'content'}, $net);
-                    }
-                    push @{$self->{'networks'}}, $wnet;
-                }
+			if (ref($xdoc->{'wireless-network'}) eq 'ARRAY') {
+				foreach my $net ( @{$xdoc->{'wireless-network'}} ) {
+					my $wnet;
+					print colored(ref($net)."\n", "bold magenta");
+					if (ref($net) eq 'HASH') {
+						if (ref($net->{'SSID'}) eq 'HASH') {
+							given (ref($net->{'SSID'}{'essid'})) {
+								when ('HASH') {
+									if (exists($net->{'SSID'}{'essid'}{'content'})) {
+										$wnet = NetXML::Wireless::Network->new($net->{'BSSID'}, $net->{'SSID'}{'essid'}{'content'}, $net);
+									} else {
+										$wnet = NetXML::Wireless::Network->new($net->{'BSSID'}, "Unknown", $net);
+									}
+									push @{$self->{'networks'}}, $wnet;
+								}
+								when ('ARRAY') {
+									print Dumper($net->{'SSID'}{'essid'});
+									exit 1;
+								}
+								default { 
+									warn colored("SSID object is either a string or nil.", "yellow");
+								}
+							}#given
+						} else { 	#if (ref($net->{'SSID'}) eq 'HASH')
+							print Dumper($net->{'SSID'});
+						}
+					} else {	#if (ref($net) eq 'HASH')
+						print Dumper($net);
+						exit 1;
+					}
+				}#foreach
             } else {
                 warn colored("Wireless network object is not an array!", "bold red");
                 print color("bold red");
